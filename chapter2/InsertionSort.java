6faa17a14989554260640cff729f2da0e2e3ff87
kaji-sugiura <kaji.sugiura@gmail.com>
2024-02-04 14:58:21 +0900

    order指定追加

--- chapter2/InsertionSort.java
+++ chapter2/InsertionSort.java
@@ -8,7 +8,15 @@ import java.util.Arrays;
  * 小さな要素に対して効率よくソートを行う
  */
 public class InsertionSort {
-    public static void sort(int[] numbers) {
+    public static void sort(int[] numbers, Order order) {
+        if (order == Order.ASC) {
+            sortByAsc(numbers);
+        } else {
+            sortByDesc(numbers);
+        }
+    }
+
+    private static void sortByAsc(int[] numbers) {
         for (int j = 1; j < numbers.length; j++) {
             int key = numbers[j];
             int i = j - 1;
@@ -20,9 +28,28 @@ public class InsertionSort {
         }
     }
 
+    private static void sortByDesc(int[] numbers) {
+        for (int j = 1; j < numbers.length; j++) {
+            int key = numbers[j];
+            int i = j - 1;
+            while (i >= 0 && numbers[i] < key) {
+                numbers[i + 1] = numbers[i];
+                i--;
+            }
+            numbers[i + 1] = key;
+        }
+    }
+
     public static void main(String[] args) {
         int[] numbers = { 4, 2, 1, 5, 6 };
-        sort(numbers);
+        sort(numbers, Order.ASC);
+        System.out.println(Arrays.toString(numbers));
+        sort(numbers, Order.DESC);
         System.out.println(Arrays.toString(numbers));
     }
+}
+
+enum Order {
+    ASC,
+    DESC
 }
