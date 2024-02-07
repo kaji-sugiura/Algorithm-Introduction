1b4fd5868fedd2df7e3b5163d32104faa60c963e
kaji-sugiura <kaji.sugiura@gmail.com>
2024-02-07 18:59:29 +0900

    bubble sort

---
+++ chapter2/BubbleSort.java
@@ -0,0 +1,23 @@
+package chapter2;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    static void sort(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = numbers.length - 1; j > i; j--) {
+                if (numbers[j] < numbers[j - 1]) {
+                    int tmp = numbers[j];
+                    numbers[j] = numbers[j - 1];
+                    numbers[j - 1] = tmp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = { 5, 2, 6, 3, 1, 9, 4, 10, Integer.MAX_VALUE, 100, -100, -20, 0 };
+        sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+}
